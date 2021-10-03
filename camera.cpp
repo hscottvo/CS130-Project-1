63742f50884dac898d378d4eedd8b61a6cce09f4
Scott <hscottvo@gmail.com>
2021-10-02 21:15:19 -0700

    Implemented World_Position in camera.cpp

--- camera.cpp
+++ camera.cpp
@@ -42,6 +42,8 @@ void Camera::Set_Resolution(const ivec2& number_pixels_input)
 vec3 Camera::World_Position(const ivec2& pixel_index)
 {
     vec3 result;
-    TODO;
+    vec2 c = Cell_Center(pixel_index);
+    result = film_position + (horizontal_vector * c[0]) + (vertical_vector * c[1]);
+    // TODO: Check if pixels are indexed by (x, y) or (row, column)
     return result;
 }
