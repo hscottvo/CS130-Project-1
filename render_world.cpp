62d718ee5c8978f15bd2fadf0de05771ac5e4258
Scott <hscottvo@gmail.com>
2021-10-02 20:03:49 -0700

    Implemented ray endpoint & direction

--- render_world.cpp
+++ render_world.cpp
@@ -29,8 +29,10 @@ Hit Render_World::Closest_Intersection(const Ray& ray)
 // set up the initial view ray and call
 void Render_World::Render_Pixel(const ivec2& pixel_index)
 {
-    TODO; // set up the initial view ray here
-    Ray ray;
+    vec3 endpoint = camera.position;
+    vec3 ray_dir = (camera.World_Position(pixel_index) - endpoint).normalized();
+
+    Ray ray = Ray(endpoint, ray_dir);
     vec3 color=Cast_Ray(ray,1);
     camera.Set_Pixel(pixel_index,Pixel_Color(color));
 }
