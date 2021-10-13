d513f3ec37c1de208f547f3e109bfbcf95ba9a86
Scott <hscottvo@gmail.com>
2021-10-13 13:17:44 -0700

    Added cmake-related files

---
+++ CMakeFiles/ray_tracer.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ray_tracer.dir/box.cpp.o"
+  "CMakeFiles/ray_tracer.dir/camera.cpp.o"
+  "CMakeFiles/ray_tracer.dir/dump_png.cpp.o"
+  "CMakeFiles/ray_tracer.dir/flat_shader.cpp.o"
+  "CMakeFiles/ray_tracer.dir/hierarchy.cpp.o"
+  "CMakeFiles/ray_tracer.dir/main.cpp.o"
+  "CMakeFiles/ray_tracer.dir/mesh.cpp.o"
+  "CMakeFiles/ray_tracer.dir/parse.cpp.o"
+  "CMakeFiles/ray_tracer.dir/phong_shader.cpp.o"
+  "CMakeFiles/ray_tracer.dir/plane.cpp.o"
+  "CMakeFiles/ray_tracer.dir/reflective_shader.cpp.o"
+  "CMakeFiles/ray_tracer.dir/render_world.cpp.o"
+  "CMakeFiles/ray_tracer.dir/sphere.cpp.o"
+  "ray_tracer"
+  "ray_tracer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ray_tracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
