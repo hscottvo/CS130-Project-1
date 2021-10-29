3852a224c66953de1e6b41ad2af9eeea345afac0
Scott <hscottvo@gmail.com>
2021-10-28 23:13:04 -0700

    Corrected reflective shader for higher recursions

--- reflective_shader.cpp
+++ reflective_shader.cpp
@@ -7,16 +7,16 @@ vec3 Reflective_Shader::
 Shade_Surface(const Ray& ray,const vec3& intersection_point,
     const vec3& normal,int recursion_depth) const
 {
-    vec3 c_0 = this->shader->Shade_Surface(ray, intersection_point, normal, recursion_depth) * (1-reflectivity);
+    vec3 c_0 = this->shader->Shade_Surface(ray, intersection_point, normal, recursion_depth);
     if (reflectivity <= small_t || recursion_depth >= world.recursion_depth_limit){
-        return c_0;
+        return c_0 * (1 - reflectivity);
     }
-    vec3 l = (intersection_point - world.camera.position).normalized();
 
-    vec3 r = (l - (2 * dot(l, normal) * normal)).normalized();
+    vec3 r = ((dot(normal, ray.direction *-1) * normal) * 2 + ray.direction);
     Ray reflect_ray = Ray(intersection_point, r);
     vec3 color_reflect = world.Cast_Ray(reflect_ray, recursion_depth + 1);
-    vec3 color = c_0 + color_reflect * pow(reflectivity, world.recursion_depth_limit-1);
+    // vec3 color = c_0 + color_reflect * pow(reflectivity, world.recursion_depth_limit-1);
+    vec3 color = c_0 + reflectivity * (color_reflect - c_0);
     
 
     return color;                             
